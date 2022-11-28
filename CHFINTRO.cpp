4011b6eff542231476689f37f2502a57846c05f6
AAYUSH KUMAR SHRIVASTAVA <114735569+Aayush894@users.noreply.github.com>
2022-11-28 09:56:14 +0530

    chef & interactive contests codechef prob. solved

---
+++ CHFINTRO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    while(a--)
+    {
+        int c;
+        cin>>c;
+        if( c >= b)
+        cout<<"Good boi"<<endl;
+        else
+        cout<<"Bad boi"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
