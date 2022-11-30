b14e3ca1b2fb8f4ce078f8420d3aa84eb07ecf69
AAYUSH KUMAR SHRIVASTAVA <114735569+Aayush894@users.noreply.github.com>
2022-11-30 17:31:02 +0530

    two rooks codechef problem solved

---
+++ TWOROOKS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t ;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if( a == c || b == d )
+        cout<<"Yes"<<endl;
+        else 
+        cout<<"No"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
