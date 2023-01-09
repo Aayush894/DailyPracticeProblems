aa18d20255091856e32fa28cfe66a65c6bd21777
AAYUSH KUMAR SHRIVASTAVA <114735569+Aayush894@users.noreply.github.com>
2023-01-09 20:41:26 +0530

    code chef problem solved

---
+++ REPEAT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t ;
+    cin>>t;
+    while(t--)
+    {
+        int n , k , s ;
+        cin>>n>>k>>s ;
+        cout<<((s - (pow(n , 2)))/(k - 1))<<endl ;
+        
+        
+    }
+	// your code goes here
+	return 0;
+}
