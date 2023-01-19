87f59b3ba83d00f60be0f50105e166121ec81ad0
AAYUSH KUMAR SHRIVASTAVA <114735569+Aayush894@users.noreply.github.com>
2023-01-19 20:21:25 +0530

    code chef problem solve

---
+++ OJUMPS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long int a ; 
+    cin>>a ;
+    
+    if(a%3 == 0 || (a-1)%6 == 0 )
+    cout<<"yes"<<endl ;
+    else 
+    cout<<"no"<<endl ; 
+	// your code goes here
+	return 0;
+}
